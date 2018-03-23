338ac2997dbcf745e1091f2bbea00a41030262d1
David Lim Cheng <davidlimcheng@gmail.com>
2018-03-22 20:57:22 -0700

    Major overhaul

--- src/certifications.js
+++
@@ -1,9 +0,0 @@
-const certifications = {
-  certification1: {
-    degree: 'Certified Associate in Project Management (CAPM)',
-    from: 'Project Management Institute',
-    date: 'December 2014'
-  }
-}
-
-export default certifications;
